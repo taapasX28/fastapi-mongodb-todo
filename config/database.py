4a9dcdb48821d297e0b20987d7bdd895f67a9b89
Taapas Agrawal <taapasagrawal@Taapass-MacBook-Air.local>
2022-01-20 19:31:31 +0530

    minor fix

--- config/database.py
+++ config/database.py
@@ -3,7 +3,7 @@ from kafka import KafkaConsumer
 from kafka import KafkaProducer
 import json
 
-client = MongoClient("mongodb+srv://Taapas:taapas@cluster0.kpoyr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+client = MongoClient("mongodb+srv://Taapas:<password>@cluster0.kpoyr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'], value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 consumer = KafkaConsumer('todo1', bootstrap_servers=['localhost:9092'], auto_offset_reset='earliest', enable_auto_commit=True, group_id='my-group' ,value_deserializer=lambda x: json.loads(x.decode('utf-8')))
 
